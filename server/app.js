b3f0bfa2ac435db0a8614619852d42d32a3bb30c
Tapabrata Goswami <tapabratagoswami21@gmail.com>
2024-07-05 23:55:10 +0530

    NodeJs Server

--- server/app.js
+++ server/app.js
@@ -2,8 +2,17 @@ import express from 'express';
 import {createServer} from 'http';
 import { Server } from 'socket.io';
 const app = express();
-
 const server = new createServer(app);
+const io = new Server(server);
 
-const io = Server(server);
+app.get('/',(req, res)=>{
+    res.send("Hi, This is back-end of chat application");
+});
+
+
+
+
+server.listen(4000, ()=>{
+    console.log(`Server running on http://localhost:4000`);
+});
 
