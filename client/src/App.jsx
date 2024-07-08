a4d5faaf30a61244f1e356287617a1531c51bb58
Tapabrata Goswami <tapabratagoswami21@gmail.com>
2024-07-09 00:15:18 +0530

    Working on server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,12 @@ function App() {
   const socket = io('http://localhost:4000');
 
   useEffect(()=>{
+    socket.on('connect',()=>{
+      console.log("Connected, Id is : ",socket.id);
+    });
+
     
+
   },[])
   return (
     <div>Chat Application</div>
