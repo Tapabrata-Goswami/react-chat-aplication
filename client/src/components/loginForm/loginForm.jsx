866ab85d6c224e263e0e47c8389ae8bf93047efa
Tapabrata Goswami <tapabratagoswami21@gmail.com>
2024-07-12 23:19:18 +0530

    initial

--- client/src/components/loginForm/loginForm.jsx
+++ client/src/components/loginForm/loginForm.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import style from './loginForm.module.css';
 
 function loginForm(){
+    const [UserName, setUserName] = useState("");
     return (
         <div className={`container ${style.login_form_container}`}>
             <div className={`card shadow p-3`}>
