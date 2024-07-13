948a63a957f5750c76846b857c1770a8fe44c259
Tapabrata Goswami <tapabratagoswami21@gmail.com>
2024-07-13 23:53:34 +0530

    initial

--- client/src/components/loginForm/loginForm.jsx
+++ client/src/components/loginForm/loginForm.jsx
@@ -7,9 +7,7 @@ function loginForm(){
         <div className={`container ${style.login_form_container}`}>
             <div className={`card shadow p-3`}>
                 <form>
-                    <div className={`form-group`}>
-                        <input type="text" className={`form-control`}/>
-                    </div>
+
                     <div className={`form-group`}>
                         <input type="password" className={`form-control`}/>
                     </div>
